691c2644909a013d4e458e855cf80cc4056f437c
Ana Maria Cojocarete <cojocarete.anamria@yahoo.ro>
2016-02-27 19:02:45 +0200

    Exercise 4

---
+++ Exercises/Exercise4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f, g, h, i, j;
+
+    cout << "Enter 10 integer values\n";
+    cin >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j;
+
+    int result = a + b + c + d + e + f + g + h + i + j;
+    cout << result << "\n";
+
+    return 0;
+}
