1e01ff480aec6c1f0a3dfebf718ac70c36ad3788
Ana Maria Cojocarete <cojocarete.anamria@yahoo.ro>
2016-02-27 18:00:28 +0200

    The first exercise

---
+++ Exercises/Exercise1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ( )
+{
+    int a;
+
+    cout <<"Enter an integer value\n";
+    cin >> a;
+            if ((a<78) && (a>56))
+            {
+    cout << "You win\n";
+
+            }
+             else
+     cout <<"You lose\n";
+
+}
+
+
+
