5f385fed9be3d52320b2151759984615a66f912f
Ana Maria Cojocarete <cojocarete.anamria@yahoo.ro>
2016-03-21 21:10:40 +0200

    ExerciseP1

---
+++ Exercises/ExerciseP1/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers a and b\n";
+    cin >> a >> b;
+
+    int *ptrA = &a;
+    int *ptrB = &b;
+
+    cout << "The value of ptrA is " << *ptrA << "\n";
+    cout << "The value of ptrB is " << *ptrB << "\n";
+
+    return 0;
+}
