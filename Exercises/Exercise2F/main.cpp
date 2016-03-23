e7cc3ac5f183c2b7f48855549e32173183a1280c
Ana Maria Cojocarete <cojocarete.anamria@yahoo.ro>
2016-03-23 23:06:12 +0200

    Exercise2F

---
+++ Exercises/Exercise2F/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void add (int a, int b, int &sum)
+{
+    sum = a + b;
+}
+
+int main()
+{
+    int no1, no2, sum;
+    cout << "Enter two numbers\n";
+    cin >> no1 >> no2;
+    add (no1, no2, sum);
+    cout << "The sum is " << sum << "\n";
+
+    return 0;
+}
