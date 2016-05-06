7dfdc846ce1343db89aee1efd7e8f7fbdbe91eaa
Ana Maria Cojocarete <cojocarete.anamria@yahoo.ro>
2016-05-06 23:34:52 +0300

    Moving the rackets

--- PongGame/MainWindow.cpp
+++ PongGame/MainWindow.cpp
@@ -9,6 +9,9 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
     , ball(BALL_START_X, BALL_START_Y, BALL_SIZE, BALL_SIZE)
+    , leftRacket(LEFT_RACKET_START_X, LEFT_RACKET_START_Y, LEFT_RACKET_WIDTH, LEFT_RACKET_HEIGHT)
+    , rightRacket(RIGHT_RACKET_START_X, RIGHT_RACKET_START_Y, RIGHT_RACKET_WIDTH, RIGHT_RACKET_HEIGHT)
+
 {
     ui->setupUi(this);
     resize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -27,7 +30,7 @@ void MainWindow::paintEvent(QPaintEvent *e) {
     doPainting();
 }
 
-void MainWindow::timerEvent(QTimerEvent *e)
+void MainWindow::timerEvent(QTimerEvent *)
 {
     moveBall();
     repaint();
@@ -41,9 +44,6 @@ void MainWindow::doPainting() {
     painter.setPen(Qt::black);
     painter.setBrush(QBrush(Qt::black));
     painter.drawRect(0, 0, table.width, table.height);
-
-    Racket leftRacket(100, 100, 20, 70);
-    Racket rightRacket(800, 100, 20, 70);
 
     painter.setPen(Qt::red);
     painter.setBrush(QBrush(Qt::red));
@@ -69,4 +69,53 @@ void MainWindow::moveBall()
         directionY *= -1;
     }
 }
+
+void MainWindow::moveRackets(int key)
+{
+    if (key == Qt::Key_Q)
+    {
+        leftRacket.positionY -= 50;
+        if (leftRacket.positionY <= 0)
+        {
+            leftRacket.positionY = 1;
+        }
+    }
+
+    if (key == Qt::Key_A)
+    {
+        leftRacket.positionY += 50;
+        if (leftRacket.positionY >= (WINDOW_HEIGHT - LEFT_RACKET_HEIGHT))
+        {
+            leftRacket.positionY = (WINDOW_HEIGHT - LEFT_RACKET_HEIGHT);
+        }
+    }
+
+    if (key == Qt::Key_Up)
+    {
+        rightRacket.positionY -= 50;
+        if (rightRacket.positionY <= 0)
+        {
+            rightRacket.positionY = 1;
+        }
+    }
+
+    if (key == Qt::Key_Down)
+    {
+        rightRacket.positionY += 50;
+        if (rightRacket.positionY >= (WINDOW_HEIGHT - RIGHT_RACKET_HEIGHT))
+        {
+            rightRacket.positionY = (WINDOW_HEIGHT - RIGHT_RACKET_HEIGHT);
+        }
+    }
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *e)
+{
+    int key = e->key();
+    moveRackets(key);
+
+    QWidget::keyPressEvent(e);
+    repaint();
+}
+
 
--- PongGame/MainWindow.h
+++ PongGame/MainWindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
 #include "Ball.h"
+#include "Racket.h"
 
 namespace Ui {
 class MainWindow;
@@ -18,23 +20,35 @@ public:
 
 protected:
     void paintEvent(QPaintEvent *e);
-    void timerEvent(QTimerEvent *e);
+    void timerEvent(QTimerEvent *);
+    void keyPressEvent(QKeyEvent *e);
 
 private:
     void doPainting();
     void moveBall();
+    void moveRackets(int key);
 
-    static const int WINDOW_WIDTH  = 1000;
-    static const int WINDOW_HEIGHT = 800;
-    static const int BALL_START_X  = 350;
-    static const int BALL_START_Y  = 135;
-    static const int BALL_SIZE     = 10;
+    static const int WINDOW_WIDTH            = 1000;
+    static const int WINDOW_HEIGHT           = 800;
+    static const int BALL_START_X            = 350;
+    static const int BALL_START_Y            = 135;
+    static const int BALL_SIZE               = 10;
+    static const int LEFT_RACKET_START_X     = 50;
+    static const int LEFT_RACKET_START_Y     = 365;
+    static const int LEFT_RACKET_WIDTH       = 20;
+    static const int LEFT_RACKET_HEIGHT      = 70;
+    static const int RIGHT_RACKET_START_X    = 930;
+    static const int RIGHT_RACKET_START_Y    = 365;
+    static const int RIGHT_RACKET_WIDTH      = 20;
+    static const int RIGHT_RACKET_HEIGHT     = 70;
 
     Ui::MainWindow *ui;
     int timerId;
     Ball ball;
     int directionX = 2;
     int directionY = 2;
+    Racket leftRacket;
+    Racket rightRacket;
 
 };
 
